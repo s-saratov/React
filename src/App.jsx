5ad5db79183d1493cd3e64c536d9f43f4359b260
s-saratov <saratovs@yandex.ru>
2025-01-20 02:20:24 +0100

    homework_02 add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Homework02 from "./homeworks/Homework_02/Homework_02";
 import Lesson02 from "./lessons/Lesson_02/Lesson_02";
 
 // Импорт по умолчанию (название компонента можно заменить на любое другое)
@@ -12,6 +13,7 @@ function App() {
       <Button />
       <Button /> */}
       <Lesson02 />
+      <Homework02 />
     </div>
   );
 }
