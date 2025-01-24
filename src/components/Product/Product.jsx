e0f6a7769e158f2cfe245aebf57ade924aaaa03e
s-saratov <saratovs@yandex.ru>
2025-01-24 01:06:35 +0100

    homework_05 improve-2

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,7 @@ import "./styles.css";
 
 function Product({ productID, productName = "Unknown", productPrice, productImg }) {
   return (
-    <div className="product-card-wrapper" key={productID}>
+    <div className="product-card-wrapper">
       <img src={productImg} className="product-img" alt="Product image" />
       <h3>{productName}</h3>
       {/* Пример тернарного оператора для скрытия элемента div, если цена не была передана */}
